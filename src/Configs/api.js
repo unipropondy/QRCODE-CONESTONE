99e6825c7352212aa9f57bacc77da9c8abc950fb
bavani18 <bavaniabavani18@gmail.com>
2026-06-24 12:02:00 +0530

    Add QR POS application

--- src/Configs/api.js
+++ src/Configs/api.js
@@ -1,7 +1,7 @@
-// export const BASE_URL = "https://new-qrcode-production-fe4e.up.railway.app"; //std
+export const BASE_URL = "https://qrcode-conestone-production.up.railway.app/"; //std
 
 
-export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000";
 
 // const BASE_URL = "https://smabackoffice-production-fc6a.up.railway.app";
 
